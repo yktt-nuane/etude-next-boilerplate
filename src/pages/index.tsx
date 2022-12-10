679109902dea6b1218976e7916cda36c49e464d5
yktt-nuane <tyinnagoyauniversity@gmail.com>
2022-12-10 17:15:50 +0900

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          etude Next.js
+        </h1>
+      </main>
+    </div>
+  )
+}
